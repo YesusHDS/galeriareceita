c5872159722b37a18e3b582fe633b5e14d70a212
YesusHDS <yesushds123@gmail.com>
2024-03-13 23:03:29 -0300

    Landing Page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,33 @@
 export default function Home() {
   return (
     <div>
-      <div className="w-[50%] border bg-black">LOGO</div>
-      <p>Seja bem vindo à galeria de receita.</p>
+      <div style={{
+        border: '2px solid black',
+        width: '50%',
 
-      <a href="#">ENTRAR</a>
+        fontWeight: 'bold',
+        fontFamily: 'arial',
+        fontSize: '48pt',
+
+        margin: '5% auto',
+        textAlign: 'center',
+        padding: '10%'
+
+      }}>LOGO</div>
+      <p style={{
+        textAlign: 'center',
+        fontSize: '24pt'
+      }}>Seja bem vindo à galeria de receita.</p>
+
+      <div style={{
+        textAlign: 'center'
+      }}>
+        <a style={{
+          fontSize: '32pt',
+          textDecoration: 'none',
+          color: 'black',
+        }} href="#">ENTRAR</a>
+      </div>
     </div>
   );
 }
